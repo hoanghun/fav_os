9e37441fbfa79aac0f639041f5d6e617a5ab02f9
Hung Hoang <hoanghun@students.zcu.cz>
2018-11-16 15:04:32 +0100

    Semaphore implementation

---
+++ src/kernel/semaphore.cpp
@@ -0,0 +1,23 @@
+#include "semaphore.h"
+
+Semaphore::Semaphore(size_t semaphore_value) : mSem_Value(semaphore_value), mQueueSize(0) {
+
+}
+
+void Semaphore::Wait() {
+	std::unique_lock<std::mutex> lock(mMutex_Lck);
+	mSem_Value--;
+	while (mSem_Value < 0) { 
+		mQueueSize++; // adding him into queue
+		mWait_Condition.wait(lock); // waiting for someone to signal the release
+	}
+}
+
+void Semaphore::Signal() {
+	std::unique_lock<std::mutex> lock(mMutex_Lck);
+	mSem_Value++;
+	if (mQueueSize > 0) { // someone is waiting, notify him
+		mQueueSize--;
+		mWait_Condition.notify_one();
+	}
+}
---
+++ src/kernel/semaphore.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+class Semaphore {
+public:
+	Semaphore(size_t semaphore_value);
+	/*
+	 * Semaphore P operation, has to wait if mSemValue <= 0
+	 */
+	void Wait();
+
+	/*
+	 * Semaphore V operation, either signals one thread waiting for lock or increases mSemValue
+	 */
+	void Signal();
+
+private:
+	std::condition_variable mWait_Condition;
+	std::mutex mMutex_Lck;
+	size_t mSem_Value;
+	size_t mQueueSize;
+};
