f1238be7f65e5a61b87b39fbde5a42976904cdb2
Jakub Vašta <vastja@gmail.com>
2018-11-27 19:02:18 +0100

    shell

--- src/user/shell.cpp
+++ src/user/shell.cpp
@@ -61,7 +61,7 @@ size_t __stdcall shell(const kiv_hal::TRegisters &regs) {
 			break;	//EOF
 
 		kiv_os_rtl::Print_Line(regs, new_line, strlen(new_line));
-	} while (strcmp(buffer, "exit") != 0 && run == true);
+	} while (strcmp(buffer, "exit") != 0 && strcmp(buffer, "shutdown") != 0);
 
 	kiv_os_rtl::Exit(0);
 
